8b826dec5a853038d2a24015900fde2b4e1d4096
Deval Panchal <devalp2401@gmail.com>
2023-12-22 10:07:56 -0500

    added responsive homepage closes #1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,75 @@
+<!-- scripts -->
 <script lang="ts">
-	import Navbar from "../components/Navbar.svelte";
-    import "../app.css";
-	import { page } from "$app/stores";
-	import { fly } from "svelte/transition";
+	import { page } from '$app/stores';
 
-    export let data;
+	import "tailwindcss/tailwind.css";
+	import "../app.css";
+	import ProfileIcon from "../components/svgs/ProfileIcon.svelte";
+	import Hamburger from "../components/svgs/Hamburger.svelte";
+	import { fly } from 'svelte/transition';
 
-    function shouldTransition(route:string) {
+	function shouldTransition(route:string) {
   	  	return route === '/' || route === '/scenario';
   	}
+	
+	export let data;
 
-    $: activeRoute = $page.url.pathname;
-    $: transitionKey = shouldTransition(data.url) ? data.url : null;
+	$: activeRoute = $page.url.pathname;
+	$: transitionKey = shouldTransition(data.url) ? data.url : null;
 </script>
 
-<!-- Layout -->
-<Navbar />
+<!-- <pre>
+	{JSON.stringify(data.url, null, 2)}
+</pre> -->
 
-{#key data.url} 
-    <div in:fly={{ x: -200, duration: 400, delay: 400 }} out:fly={{ x: 200, duration: 400 }}>
-        <slot />
-    </div>
+<!-- layout -->
+<nav class="navbar bg-neutral text-neutral-content">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+				<Hamburger />
+			</div>
+			<ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-neutral text-neutral-content">
+				<li class="text-xl">
+					<a href="/" class:primary-bg={$page.url.pathname === '/'}>Home</a>
+				</li>
+				<li class="text-xl">
+					<a href="/scenario" class:primary-bg={$page.url.pathname.includes('/scenario')}>Scenario</a>
+				</li>
+				<li class="text-xl">
+					<a href="/achievements" class:primary-bg={$page.url.pathname === '/achievements'}>Achievements</a>
+				</li>
+				<li class="text-xl">
+					<a href="/story" class:primary-bg={$page.url.pathname === '/story'}>Create new scenario</a>
+				</li>
+			</ul>
+		</div>
+		<a href="/" class="btn btn-ghost text-xl">LingoComics</a>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<ul class="menu menu-horizontal px-1">
+			<li class="text-xl px-1">
+				<a href="/" class="transition-bg" class:primary-bg={$page.url.pathname === '/'}>Home</a>
+			</li>
+			<li class="text-xl px-1">
+				<a href="/scenario" class="transition-bg" class:primary-bg={$page.url.pathname.includes('/scenario')}>Scenario</a>
+			</li>
+			<li class="text-xl px-1">
+				<a href="/achievements" class="transition-bg" class:primary-bg={$page.url.pathname === '/achievements'}>Achievements</a>
+			</li>
+			<li class="text-xl px-1">
+				<a href="/story" class="transition-bg" class:primary-bg={$page.url.pathname === '/story'}>Create new scenario</a>
+			</li>
+		</ul>
+	</div>
+	<div class="navbar-end">
+		<ProfileIcon />
+	</div>
+</nav>
+
+
+{#key data.url}	
+	<div in:fly={{ x: -200, duration: 400, delay: 400 }} out:fly={{ x: 200, duration: 400 }}>
+		<slot />
+	</div>
 {/key}
-<!-- Layout -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import comicBookImage from '../assets/images/home_comic_img.png';
+</script>
+
+<div class="flex flex-col lg:flex-row justify-center items-center max-w-7xl mx-auto p-4 h-screen">
+    <div class="lg:flex-1 px-4 py-2 text-left">
+        <h1 class="text-6xl pb-10">LingoComics</h1>
+        <p class="text-2xl pb-10">Start your language learning journey with a new interactive way</p>
+        <a href="/scenario" class="btn custom-btn-bg shadow text-2xl px-10 w-1/2 hover:bg-transparent hover:text-black">Start</a>
+    </div>
+    <div class="lg:flex-1 px-4 py-2 text-center">
+        <img src={comicBookImage} alt="comic book" class="max-w-full h-auto" />
+    </div>
+</div>
